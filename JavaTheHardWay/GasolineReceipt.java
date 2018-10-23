9d74cc6880c9da0c77075f29e878436bc3a1976e
Braiden Miller <braidenjamesmiller@gmail.com>
2018-10-22 18:53:28 -0600

    finish jthw ex 2

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+  public static void main( String[] args ) {
+    System.out.println( "+------------------------+" );
+    System.out.println( "|                        |" );
+    System.out.println( "|      Corner Store      |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| 2018-10-22  04:20pm    |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| Gallons:      10.870   |" );
+    System.out.println( "| Price/gallpn: $ 2. 89  |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| Fuel total: $ 22.71    |" );
+    System.out.println( "|                        |" );
+    System.out.println( "+------------------------+" );
+  }
+}
