5d05c30f5a898d68f68f03c4d76d3f119bdda6ba
Braiden Miller <braidenjamesmiller@gmail.com>
2018-10-22 23:36:32 -0600

    completed Objective1Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,6 @@
+public class Objective1Lab3{
+  public static void main(String[] args){
+    System.out.println( "Name : Braiden Miller" );
+    System.out.println( "Age : 27\nState : Utah" ); //wanted to pratice this 
+  }
+}
