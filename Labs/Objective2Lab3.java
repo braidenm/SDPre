401c82c91e3f0b5702836326473ef6f9eca2fec4
Braiden Miller <braidenjamesmiller@gmail.com>
2018-10-24 14:42:12 -0600

    finshed Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3 {
+  public static void main(String[] args ) {
+    String food1, food2, food3;
+    food1 = "Pizza";
+    food2 = "Sushi";
+    food3 = "Breakfast Burritos";
+
+    System.out.println( "My favorite foods are:" );
+    System.out.println( "1: " + food1 );
+    System.out.println( "2: " + food2 );
+    System.out.println( "3: " + food3 );
+  }
+}
