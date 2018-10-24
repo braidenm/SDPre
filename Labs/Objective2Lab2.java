cf14596cfad16ac9168d0aaaf473699465fea784
Braiden Miller <braidenjamesmiller@gmail.com>
2018-10-24 14:29:15 -0600

    finished Obejective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args ) {
+    String name = "Braiden Miller";
+    int age = 27;
+    String state = "Utah";
+
+    System.out.println( "Name: " + name + "\nAge: " + age + "\nState: " + state );
+    //wanted to see if I could do this correctly on one line :)
+  }
+}
