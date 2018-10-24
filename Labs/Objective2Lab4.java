af9c98d542af3bbe6aad68fed44c58483019f6c1
Braiden Miller <braidenjamesmiller@gmail.com>
2018-10-24 14:53:42 -0600

    finished Objective2Lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,12 @@
+public class Objective2Lab4 {
+  public static void main(String[] args ) {
+    double num1, num2, num3, average;
+    num1 = 5;
+    num2 = 8;
+    num3 = 4;
+    average = num1 + num2 + num3;
+
+    System.out.println( "This output would result from numbers: 5, 8, 4:" );
+    System.out.println( average/3 );
+  }
+}
