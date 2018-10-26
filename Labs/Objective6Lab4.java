6390109468fdd171a3e1cb4a797e20bbcfba9c4a
Braiden Miller <braidenjamesmiller@gmail.com>
2018-10-26 17:02:58 -0600

    finished Objective6Lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,14 @@
+public class Objective6Lab4 {
+  public static void main(String[] args ){
+    int count = 1, sum = 0;
+
+    while (count <= 20){
+
+      sum = count + sum;
+      count = count + 1;
+
+
+    }
+    System.out.println(sum);
+  }
+}
