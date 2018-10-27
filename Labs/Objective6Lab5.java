be277a323c85708e0510340e6b6b921173d0fd51
Braiden Miller <braidenjamesmiller@gmail.com>
2018-10-26 18:01:51 -0600

    finished Objective6Lab5

---
+++ Labs/Objective6Lab5.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Objective6Lab5 {
+  public static void main(String[] args ){
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+
+    while (true) {
+      System.out.println( "_____Menu_____");
+      System.out.println("1: Say Hello");
+      System.out.println("2: List My favorite foods");
+      System.out.println("3: Exit");
+      System.out.println();
+      System.out.print("Selection: ");
+      selection = scanner.nextInt();
+
+      if (selection == 1) {
+        System.out.println( "Hello Human");
+        System.out.println();
+      }
+      else if (selection == 2) {
+        System.out.println("Pizza, Beer, Wings");
+        System.out.println();
+      }
+      else if (selection == 3){
+        System.out.println("Goodbye");
+        break;
+      }
+
+      }
+    }
+  }
