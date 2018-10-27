162ed46cc868f71e95a9242e16d8b3acebea313c
Braiden Miller <braidenjamesmiller@gmail.com>
2018-10-27 16:28:29 -0600

    finihsed SD2 ex 9

--- JavaTheHardWay/BMICalculator.java
+++ JavaTheHardWay/BMICalculator.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 public class BMICalculator {
   public static void main( String[] args ) {
     Scanner keyboard = new Scanner(System.in);
-    double m, kg, bmi, i, lb;
+    double m, kg, bmi, i, lb, ft, in;
 
-    System.out.print( "Your height in inches: " );
+    System.out.print( "Your height (feet only): ");
+    ft = keyboard.nextDouble();
+
+    System.out.print( "Your height (inches): " );
     i = keyboard.nextDouble();
 
     System.out.print( "Your weight in pounds: " );
     lb = keyboard.nextDouble();
-    m = i/39.37;
+
+    in= ft*12+i;
+    m = in/39.37;
     kg = lb/2.205;
     bmi = kg / (m*m);
 
